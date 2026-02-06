d0d3366f5c97b266808c0e9a5ae7f48d7f769060
Gustavo Ribeiro <tumafojuapo@gmail.com>
2026-02-06 03:29:34 +0000

    Configuração para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], base: '/meu-app-android/',
   base: '/meu-app-android/', // IMPORTANTE para GitHub Pages
   build: {
     outDir: 'dist',
