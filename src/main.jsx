125cbc66f38dda763487f7e46a2058a873b5cd81
Gustavo Ribeiro <tumafojuapo@gmail.com>
2026-02-15 01:46:23 +0000

    Add: Contador de acessos no Supabase

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseCounter = createClient("https://npmdvkggsklkideqoriw.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wbWR2a2dnc2tsa2lkZXFvcml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NzMyMDAsImV4cCI6MjA4NjE0OTIwMH0.y-X0NS-_9BV7RhtSUOteLhaUPnt8Tkf24NlUikR8Ifo");
+
+if (!sessionStorage.getItem("visited")) {
+  supabaseCounter.from("visitor_counter").insert({}).then(() => sessionStorage.setItem("visited", "true"));
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
